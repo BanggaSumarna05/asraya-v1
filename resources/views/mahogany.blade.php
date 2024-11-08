66513e77844a44942e88a29a3b0118e55d543043
0nvendra <onvendra@gmail.com>
2024-11-08 10:48:32 +0700

    q

--- resources/views/mahogany.blade.php
+++ resources/views/mahogany.blade.php
@@ -57,7 +57,7 @@
                     </div>
                 </div>
                 <div class="col-7" style="left: 14px;">
-                    <img class="image-fluid w-100 h-100 lazy float-right" src="img/mahogany/mahogany-interior.jpg">
+                    <img class="image-fluid w-100 h-100 lazy float-right" src="img/mahogany/mahogany-interior-0.jpg">
                 </div>
             </div>
             <div class="row" {{-- secondary color --}} style="background-color: white;">
