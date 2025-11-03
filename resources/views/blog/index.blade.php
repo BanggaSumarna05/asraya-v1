032a4637811f8c6d8585145e9ccc62aa6bba48a0
0nvendra <onvendra@gmail.com>
2025-11-03 10:13:55 +0700

    q

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -12,13 +12,13 @@
 
         <h1 class="text-center mb-4 fw-bold">Berita & Artikel</h1>
         <p class="text-center text-muted mb-5">
-            Inspirasi seputar hunian, desain, dan gaya hidup.
+            Inspirasi seputar Perumahan, Hunian, Desain & Lifestyle.
         </p>
 
         <div class="row">
             @foreach ($posts as $post)
-                <div class="col-4 col-sm-6 col-md-4 col-lg-4 ">
-                    <a href="{{ route('blog.show', $post->slug) }}" class="text-decoration-none text-dark gx-4 gy-4">
+                <div class="col-4 col-sm-6 col-md-4 col-lg-4 pb-3 pt-3">
+                    <a href="{{ route('blog.show', $post->slug) }}" class="text-decoration-none text-dark">
                         <div class="card shadow-sm h-100 border-0">
                             @if ($post->image)
                                 <img src="{{ asset('storage/' . $post->image) }}" class="card-img-top"
@@ -39,11 +39,12 @@
                 </div>
             @endforeach
         </div>
-        <div class="mt-5 d-flex justify-content-center">
+        <div class="mt-5  mb-5 d-flex justify-content-center">
             <style>
-                /* kecilkan ukuran teks dan ikon pada pagination */
-                .pagination { font-size: 0.9rem; }
-                .pagination .page-link svg { width: 1em; height: 1em; }
+                svg.w-5.h-5 {
+                    width: 22px !important;
+                    height: 22px !important;
+                }
             </style>
 
             {{ $posts->links() }}
