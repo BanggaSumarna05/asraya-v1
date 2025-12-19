fd4ebbc7109c43e8baa7e884e6beefd17bddf420
0nvendra <onvendra@gmail.com>
2025-12-19 16:43:32 +0700

    q

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -59,12 +59,11 @@
                             <li class="has-children">
                                 <a href="#">Profil Kami</a>
                                 <ul class="dropdown">
-                                    <li><a href="assets/ebrochure/asraya-22092025.pdf" target="_blank">E-Brochure</a>
-                                    </li>
+                                    <li><a href="assets/ebrochure/asraya-brosur.pdf" target="_blank">E-Brochure</a></li>
                                     <li><a href="assets/ebrochure/asraya-profile.pdf" target="_blank">E-Profile</a></li>
-                                    <li><a href="assets/ebrochure/site_plan_asraya_property.pdf" target="_blank">Site
-                                            Plan Available</a></li>
-                                    <li><a href="https://linktr.ee/casasraya" target="_blank">Linktree</a></li>
+                                    <li><a href="assets/ebrochure/site_plan_asraya_property.png" target="_blank">Site Plan
+                                            Available</a></li>
+                                    <li><a href="https://linktr.ee/casaasraya" target="_blank">Linktree</a></li>
                                 </ul>
                             </li>
                             <li><a href="{{ route('galeri') }}">Galeri</a></li>
